765349870d12a6bfa41587dd4788c07afa062479
Alfian Isnan <fianshared@gmail.com>
2022-06-10 15:58:06 +0700

    Guest for LDAP as a normal user

--- sso/api/account/ldap.py
+++ sso/api/account/ldap.py
@@ -61,7 +61,6 @@ class LDAP:
         return error
 
     def update_user(self, user):
-        if(user.user_type == "GUEST"): return False
         self.bind()
         dn, ldif =  ldif_mailuser(user)
         try:
